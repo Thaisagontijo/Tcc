a11ce6479aefd480724d8c432177b197b60c80d8
Thaisa <Thaisa@Thaisa-PC>
2013-10-13 20:19:57 -0300

    Criacao da Classe cidade no DomainModel

---
+++ Tcc/src/br/com/tcc/DomainModel/Cidade.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Modric
+ */
+@Entity
+public class Cidade implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    
+}
---
+++ Tcc/src/br/com/tcc/DomainModel/Pessoa.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Thaisa
+ */
+@Entity
+@Table(name="pessoas")
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    private String rg;
+    private String cpf;
+    private String observacao;
+    private String telefone;
+    private String celular;
+    private String enderecoRua;
+    private int enderecoNumero;
+    private String enderecoBairro;
+    
+    
+    
+    
+}
