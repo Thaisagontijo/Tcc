ebac2bc364ca59cbb5bf844a29914805a651bc0c
Thaisa <Thaisa@Thaisa-PC>
2013-10-13 20:46:16 -0300

    Criacao da Classe Fornecedores no DomainModel

---
+++ Tcc/src/br/com/tcc/DomainModel/Fornecedores.java
@@ -0,0 +1,240 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.DomainModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Thaisa
+ */
+@Entity
+@Table(name="Fornecedores")
+public class Fornecedores implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Fornecedores() {
+        dataCadastro = new Date();
+    }
+    
+    
+    
+    
+    private String nome;
+    private String telefone;
+    private String celular;
+    private String email;
+    private String site;
+    private String razaoSocial;
+    private String cnpj;
+    private String inscricaoEstadual;
+    private String incricaoMunicipal;
+    private String observacoes;
+    private String enderecoRua;
+    private int enderecoNumero;
+    private String enderecoCep;
+    private String enderecoBairro;
+    private String enderecoComplemento;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataCadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getIncricaoMunicipal() {
+        return incricaoMunicipal;
+    }
+
+    public void setIncricaoMunicipal(String incricaoMunicipal) {
+        this.incricaoMunicipal = incricaoMunicipal;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public String getEnderecoRua() {
+        return enderecoRua;
+    }
+
+    public void setEnderecoRua(String enderecoRua) {
+        this.enderecoRua = enderecoRua;
+    }
+
+    public int getEnderecoNumero() {
+        return enderecoNumero;
+    }
+
+    public void setEnderecoNumero(int enderecoNumero) {
+        this.enderecoNumero = enderecoNumero;
+    }
+
+    public String getEnderecoCep() {
+        return enderecoCep;
+    }
+
+    public void setEnderecoCep(String enderecoCep) {
+        this.enderecoCep = enderecoCep;
+    }
+
+    public String getEnderecoBairro() {
+        return enderecoBairro;
+    }
+
+    public void setEnderecoBairro(String enderecoBairro) {
+        this.enderecoBairro = enderecoBairro;
+    }
+
+    public String getEnderecoComplemento() {
+        return enderecoComplemento;
+    }
+
+    public void setEnderecoComplemento(String enderecoComplemento) {
+        this.enderecoComplemento = enderecoComplemento;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.nome);
+        hash = 71 * hash + Objects.hashCode(this.telefone);
+        hash = 71 * hash + Objects.hashCode(this.celular);
+        hash = 71 * hash + Objects.hashCode(this.email);
+        hash = 71 * hash + Objects.hashCode(this.site);
+        hash = 71 * hash + Objects.hashCode(this.cnpj);
+        hash = 71 * hash + Objects.hashCode(this.inscricaoEstadual);
+        hash = 71 * hash + Objects.hashCode(this.incricaoMunicipal);
+        hash = 71 * hash + Objects.hashCode(this.observacoes);
+        hash = 71 * hash + Objects.hashCode(this.enderecoRua);
+        hash = 71 * hash + this.enderecoNumero;
+        hash = 71 * hash + Objects.hashCode(this.enderecoCep);
+        hash = 71 * hash + Objects.hashCode(this.enderecoBairro);
+        hash = 71 * hash + Objects.hashCode(this.enderecoComplemento);
+        hash = 71 * hash + Objects.hashCode(this.dataCadastro);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fornecedores other = (Fornecedores) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cnpj, other.cnpj)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataCadastro, other.dataCadastro)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return  nome;
+    }
+ 
+}
