77007081b3aec7dfba0a4428dbfeb0943969fa3a
Thaisa <Thaisa@Thaisa-PC>
2013-10-13 20:26:22 -0300

    Criacao da Classe Pessoa no DomainModel

--- Tcc/src/br/com/tcc/DomainModel/Pessoa.java
+++ Tcc/src/br/com/tcc/DomainModel/Pessoa.java
@@ -5,11 +5,14 @@
 package br.com.tcc.DomainModel;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 
 /**
  *
@@ -23,6 +26,16 @@ public class Pessoa implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    public Pessoa() {
+        dataNascimento = new Date();
+    }
+    
+    
+    
+    
+    
+    
+
     private String nome;
     private String rg;
     private String cpf;
@@ -32,8 +45,168 @@ public class Pessoa implements Serializable {
     private String enderecoRua;
     private int enderecoNumero;
     private String enderecoBairro;
-    
-    
-    
-    
+    private String enderecoComplemento;
+    private String enderecoCep;
+    private int sexo;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEnderecoRua() {
+        return enderecoRua;
+    }
+
+    public void setEnderecoRua(String enderecoRua) {
+        this.enderecoRua = enderecoRua;
+    }
+
+    public int getEnderecoNumero() {
+        return enderecoNumero;
+    }
+
+    public void setEnderecoNumero(int enderecoNumero) {
+        this.enderecoNumero = enderecoNumero;
+    }
+
+    public String getEnderecoBairro() {
+        return enderecoBairro;
+    }
+
+    public void setEnderecoBairro(String enderecoBairro) {
+        this.enderecoBairro = enderecoBairro;
+    }
+
+    public String getEnderecoComplemento() {
+        return enderecoComplemento;
+    }
+
+    public void setEnderecoComplemento(String enderecoComplemento) {
+        this.enderecoComplemento = enderecoComplemento;
+    }
+
+    public String getEnderecoCep() {
+        return enderecoCep;
+    }
+
+    public void setEnderecoCep(String enderecoCep) {
+        this.enderecoCep = enderecoCep;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.nome);
+        hash = 89 * hash + Objects.hashCode(this.rg);
+        hash = 89 * hash + Objects.hashCode(this.cpf);
+        hash = 89 * hash + Objects.hashCode(this.observacao);
+        hash = 89 * hash + Objects.hashCode(this.telefone);
+        hash = 89 * hash + Objects.hashCode(this.celular);
+        hash = 89 * hash + Objects.hashCode(this.enderecoRua);
+        hash = 89 * hash + this.enderecoNumero;
+        hash = 89 * hash + Objects.hashCode(this.enderecoBairro);
+        hash = 89 * hash + Objects.hashCode(this.enderecoComplemento);
+        hash = 89 * hash + Objects.hashCode(this.enderecoCep);
+        hash = 89 * hash + this.sexo;
+        hash = 89 * hash + Objects.hashCode(this.dataNascimento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.rg, other.rg)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
 }
