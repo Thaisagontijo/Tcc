58f6f54068de838148c54a08b34c789d8dba822b
Thaisa <Thaisa@Thaisa-PC>
2013-10-14 18:06:50 -0300

    Criacao da Classe Funcionario

---
+++ Tcc/src/br/com/tcc/DomainModel/Funcionario.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.DomainModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Modric
+ */
+@Entity
+public class Funcionario extends Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Funcionario() {
+        this.dataAdmissao = new Date();
+        this.cargos = new LinkedList();
+    }
+
+    
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataAdmissao;
+    
+    private List<Cargo> cargos;
+    
+    private void addCargo(Cargo c){
+        if(!cargos.contains(c)){
+            this.cargos.add(c);
+        }
+    }
+    
+    private void removeCargo(Cargo c){
+        if(cargos.contains(c)){
+            cargos.remove(c);
+        }
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<Cargo> cargos) {
+        this.cargos = cargos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.dataAdmissao);
+        hash = 89 * hash + Objects.hashCode(this.cargos);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Funcionario other = (Funcionario) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataAdmissao, other.dataAdmissao)) {
+            return false;
+        }
+        if (!Objects.equals(this.cargos, other.cargos)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
