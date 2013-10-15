00b907d2b40d2e9e0d2801224e5f47f995132aef
Thaisa <Thaisa@Thaisa-PC>
2013-10-15 15:25:59 -0300

    Criacao da Classe FormaDePagamentoDAO

---
+++ Tcc/src/br/com/tcc/DataAccess/FormaDePagamentoDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tcc.DataAccess;
+
+import br.com.tcc.DomainModel.FormaDePagamento;
+
+/**
+ *
+ * @author Modric
+ */
+public class FormaDePagamentoDAO extends DAOGenerico<FormaDePagamento> {
+
+    public FormaDePagamentoDAO(){
+        super(FormaDePagamento.class);
+    }
+    
+    
+}
